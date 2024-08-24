1336bb4187f2797cd0cfd22e24dcaf060325000b
gleecus <teja.ulisetti@gleecus.com>
2024-08-24 22:59:16 +0530

    feat:implement tags uisng createElement and CDNs

---
+++ App.js
@@ -0,0 +1,10 @@
+const parent = React.createElement("div", { id: "parent" },
+    React.createElement("div", { id: "child" },
+        [
+            React.createElement("h1", { key: "t1" }, "I am Heading-1"),
+            React.createElement("h1", { key: "t2" }, "I am Heading-2")
+        ]
+    )
+)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
